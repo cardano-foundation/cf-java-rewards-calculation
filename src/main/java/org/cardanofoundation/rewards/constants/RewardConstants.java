014bbea41012c6cb26b1b4d3f5c46c538e8cdea3
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2023-09-06 23:34:49 +0200

    fix: adjust epochs, subtract pool fees, calculate ada in circulation with reserve from next epoch

--- src/main/java/org/cardanofoundation/rewards/constants/RewardConstants.java
+++ src/main/java/org/cardanofoundation/rewards/constants/RewardConstants.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class RewardConstants {
-  public static final BigDecimal TOTAL_ADA = new BigDecimal("45000000000000000");
+  public static final BigDecimal TOTAL_LOVELACE = new BigDecimal("45000000000000000");
   public static final BigInteger DEPOSIT_POOL_REGISTRATION_IN_LOVELACE = BigInteger.valueOf(500000000L);
   // https://developers.cardano.org/docs/operate-a-stake-pool/introduction-to-cardano/#slots-and-epochs
   public static final int EXPECTED_SLOT_PER_EPOCH = 432000;
