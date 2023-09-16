718ed01da5b99dcd6d78a3f8d5b002a6fc80e321
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2023-09-16 22:47:58 +0200

    feat: add reporting and export the report to GH pages

--- src/main/java/org/cardanofoundation/rewards/calculation/TreasuryCalculation.java
+++ src/main/java/org/cardanofoundation/rewards/calculation/TreasuryCalculation.java
@@ -1,6 +1,13 @@
 package org.cardanofoundation.rewards.calculation;
 
+import org.cardanofoundation.rewards.data.provider.DataProvider;
+import org.cardanofoundation.rewards.entity.AdaPots;
+import org.cardanofoundation.rewards.entity.Epoch;
+import org.cardanofoundation.rewards.entity.ProtocolParameters;
+import org.cardanofoundation.rewards.entity.TreasuryCalculationResult;
+
 import static org.cardanofoundation.rewards.constants.RewardConstants.EXPECTED_SLOT_PER_EPOCH;
+import static org.cardanofoundation.rewards.util.CurrencyConverter.lovelaceToAda;
 
 public class TreasuryCalculation {
 
@@ -54,5 +61,60 @@ public class TreasuryCalculation {
     // the expectation value of blocks that should have been produced during the epoch under
     // ideal conditions.
     return Math.min(1, totalBlocksInEpochByPools / expectedBlocksInNonOBFTSlots);
+  }
+
+  public static TreasuryCalculationResult calculateTreasuryForEpoch(int epoch, DataProvider dataProvider) {
+    ProtocolParameters protocolParameters = dataProvider.getProtocolParametersForEpoch(epoch - 2);
+    final double treasuryGrowthRate = protocolParameters.getTreasuryGrowRate();
+    final double monetaryExpandRate = protocolParameters.getMonetaryExpandRate();
+    double decentralizationParameter = protocolParameters.getDecentralisation();
+
+    AdaPots adaPotsForPreviousEpoch = dataProvider.getAdaPotsForEpoch(epoch - 1);
+    AdaPots adaPotsForCurrentEpoch = dataProvider.getAdaPotsForEpoch(epoch);
+
+    // The Shelley era and the ada pot system started on mainnet in epoch 208.
+    // Fee and treasury values are 0 for epoch 208.
+    double totalFeesForCurrentEpoch = 0.0;
+    Epoch epochInfo = dataProvider.getEpochInfo(epoch - 2);
+    if (epoch > 209) {
+      totalFeesForCurrentEpoch = epochInfo.getFees();
+    }
+
+    double reserveInPreviousEpoch = adaPotsForPreviousEpoch.getReserves();
+
+    double treasuryInPreviousEpoch = adaPotsForPreviousEpoch.getTreasury();
+    double expectedTreasuryForCurrentEpoch = adaPotsForCurrentEpoch.getTreasury();
+
+    int totalBlocksInEpoch = epochInfo.getBlockCount();
+
+    if (epoch > 214 && epoch < 257) {
+      Epoch currentEpochInfo = dataProvider.getEpochInfo(epoch - 2);
+      totalBlocksInEpoch = currentEpochInfo.getNonOBFTBlockCount();
+    }
+
+    double rewardPot = TreasuryCalculation.calculateTotalRewardPotWithEta(
+            monetaryExpandRate, totalBlocksInEpoch, decentralizationParameter, reserveInPreviousEpoch, totalFeesForCurrentEpoch);
+
+    double treasuryForCurrentEpoch = TreasuryCalculation.calculateTreasury(
+            treasuryGrowthRate, rewardPot, treasuryInPreviousEpoch);
+
+    /*
+      TODO: "For each retiring pool, the refund for the pool registration deposit is added to the
+       pool's registered reward account, provided the reward account is still registered." -
+       https://github.com/input-output-hk/cardano-ledger/blob/9e2f8151e3b9a0dde9faeb29a7dd2456e854427c/eras/shelley/formal-spec/epoch.tex#L546C9-L547C87
+
+
+       int retiredPoolsWithDeregisteredRewardAddress = koiosDataProvider.countRetiredPoolsWithDeregisteredRewardAddress(epoch - 1);
+
+       BigDecimal deposit = new BigDecimal(DEPOSIT_POOL_REGISTRATION_IN_ADA);
+       treasuryForCurrentEpoch = treasuryForCurrentEpoch.add(deposit.multiply(new BigDecimal(retiredPoolsWithDeregisteredRewardAddress)));
+    */
+
+    return TreasuryCalculationResult.builder()
+            .calculatedTreasury(treasuryForCurrentEpoch)
+            .actualTreasury(expectedTreasuryForCurrentEpoch)
+            .epoch(epoch)
+            .totalRewardPot(rewardPot)
+            .build();
   }
 }
