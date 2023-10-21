a7714760c5e1db56a8af49c4b8b3f551943aeae6
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2023-10-21 09:21:41 +0200

    chore: implement AdaPots mapping for dbsync data provider

--- src/test/java/org/cardanofoundation/rewards/data/provider/DbSyncDataProviderTest.java
+++ src/test/java/org/cardanofoundation/rewards/data/provider/DbSyncDataProviderTest.java
@@ -1,5 +1,6 @@
 package org.cardanofoundation.rewards.data.provider;
 
+import org.cardanofoundation.rewards.entity.AdaPots;
 import org.cardanofoundation.rewards.entity.Epoch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -25,5 +26,14 @@ public class DbSyncDataProviderTest {
         Assertions.assertEquals(epoch.getNumber(), 220);
         Assertions.assertEquals(epoch.getFees(), 5135934788.0);
         Assertions.assertEquals(epoch.getBlockCount(), 21627);
+    }
+
+    @Test
+    public void testGetAdaPots() {
+        AdaPots adaPots = dbSyncDataProvider.getAdaPotsForEpoch(220);
+        Assertions.assertEquals(adaPots.getTreasury(), 9.4812346026398E13);
+        Assertions.assertEquals(adaPots.getReserves(), 1.3120582265809832E16);
+        Assertions.assertEquals(adaPots.getRewards(), 1.51012138061367E14);
+        //Assertions.assertEquals(adaPots.getAdaInCirculation(), 3.005841773419017E16);
     }
 }
