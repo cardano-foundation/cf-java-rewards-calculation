edd65e85720055f6417dbc9df918bd86092bc75f
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2023-11-09 11:45:02 +0100

    feat: finish the implementation of the dbsync data provider

--- src/main/java/org/cardanofoundation/rewards/repository/DbSyncBlockRepository.java
+++ src/main/java/org/cardanofoundation/rewards/repository/DbSyncBlockRepository.java
@@ -15,4 +15,17 @@ public interface DbSyncBlockRepository extends ReadOnlyRepository<DbSyncBlock, L
                 and block.slotLeader.pool.bech32PoolId = :poolId""")
     Integer getBlocksMadeByPoolInEpoch(String poolId, Integer epoch);
 
+   @Query("""
+              select count(*) from DbSyncBlock AS block
+                    where block.epochNo = :epoch
+                    and block.slotLeader.pool is NOT NULL
+           """)
+    Integer getNonOBFTBlocksInEpoch(Integer epoch);
+
+    @Query("""
+              select count(*) from DbSyncBlock AS block
+                    where block.epochNo = :epoch
+                    and block.slotLeader.pool is NULL
+           """)
+    Integer getOBFTBlocksInEpoch(Integer epoch);
 }
