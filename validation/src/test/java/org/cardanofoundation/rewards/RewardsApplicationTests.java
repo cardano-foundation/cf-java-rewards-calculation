f8b1bef46b091a9d56fcedc604a5a4cec94baf53
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2024-03-02 19:03:15 +0100

    chore: add plugin to locate and run tests

--- validation/src/test/java/org/cardanofoundation/rewards/RewardsApplicationTests.java
+++ validation/src/test/java/org/cardanofoundation/rewards/RewardsApplicationTests.java
@@ -1,10 +1,11 @@
 package org.cardanofoundation.rewards;
 
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = RewardsApplication.class)
+@TestPropertySource(locations = { "classpath:application.yaml" })
 class RewardsApplicationTests {
   @Test
   void contextLoads() {}
