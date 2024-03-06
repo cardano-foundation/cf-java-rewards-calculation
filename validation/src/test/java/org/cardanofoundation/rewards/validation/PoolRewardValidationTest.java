fa9621d6756405cd90d063232c0c9b90e9b6f650
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2024-03-06 22:44:22 +0100

    chore: repair test range

--- validation/src/test/java/org/cardanofoundation/rewards/validation/PoolRewardValidationTest.java
+++ validation/src/test/java/org/cardanofoundation/rewards/validation/PoolRewardValidationTest.java
@@ -96,10 +96,11 @@ public class PoolRewardValidationTest {
     }
 
     @Test
+    @EnabledIf(expression = "#{environment.acceptsProfiles('db-sync')}", loadContext = true, reason = "DB Sync data provider must be available for this test")
     void calculateEVE3PoolRewardInEpoch363() {
         String poolId = "pool1kchver88u3kygsak8wgll7htr8uxn5v35lfrsyy842nkscrzyvj";
         int epoch = 363;
-        Test_calculatePoolReward(poolId, epoch, DataProviderType.JSON);
+        Test_calculatePoolReward(poolId, epoch, DataProviderType.DB_SYNC);
     }
 
     @Test
